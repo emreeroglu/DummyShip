4c27c169ed0a7ed70014d55dd6727721d5c3f451
Emre Eroglu <emre@emreeroglu.net>
2017-03-05 05:09:56 +0300

    Add bullet counter to window title.

--- bin/game.py
+++ bin/game.py
@@ -12,7 +12,7 @@ class Bullet(Label):
     def __init__(self, x, y, space):
         self.space = space
         self.bullet_timer = 0.01
-        self.bullet_indicator = "."
+        self.bullet_indicator = "`"
         Label.__init__(self, text=self.bullet_indicator)
         self.pack()
         process = threading.Thread(target=self.place_bullet(x=x, y=y))
@@ -42,7 +42,8 @@ class SpaceShip(Label):
         self.pack()
         self.place_ship()
         self.life = 100
-        self.bullet_count = 3
+        self._observers = []
+        self._bullet_count = 0
 
     def left_key(self, event):
         self.x -= 10
@@ -80,33 +81,48 @@ class SpaceShip(Label):
     def shoot(self):
         if self.bullet_count > 0:
             Bullet(x=self.x, y=self.y, space=self.space)
-            self.bullet_count -= 1
+            self.bullet_count = -1
+
+    def get_bullet_count(self):
+        return self._bullet_count
+
+    def set_bullet_count(self, value):
+        self._bullet_count += value
+        for callback in self._observers:
+            callback(self._bullet_count)
+
+    bullet_count = property(get_bullet_count, set_bullet_count)
+
+    def bind_to(self, callback):
+        self._observers.append(callback)
 
 
 class DummyGame(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
+        # Set game window arguments.
+        self.parent = parent
+        self.pack(fill=BOTH, expand=1)
+        self.bullet_count = 0
+
         # Set space ship.
         self.space = Space()
         self.space_ship = SpaceShip(space=self.space)
+        self.space_ship.bind_to(self.set_title)
+        self.space_ship.bullet_count = 3
+
         parent.bind('<Left>', self.space_ship.left_key)
         parent.bind('<Right>', self.space_ship.right_key)
         parent.bind('<Up>', self.space_ship.up_key)
         parent.bind('<Down>', self.space_ship.down_key)
         parent.bind('<space>', self.space_ship.space_key)
 
-        # Set game window arguments.
-        self.parent = parent
-        self.pack(fill=BOTH, expand=1)
-        self.set_title()
+    def set_title(self, bullet_count):
+        if bullet_count:
+            self.bullet_count = bullet_count
+        self.parent.title("Dummy Space Ship - Bullet: " + str(self.bullet_count))
 
-    def set_title(self):
-        self.parent.title("Dummy Ship Game")
-
-
-
-            
 
 def main():
     root = Tk()
@@ -116,5 +132,4 @@ def main():
     
 if __name__ == '__main__':
     main()
-    
 
